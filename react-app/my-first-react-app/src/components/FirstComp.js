81c5e42d012a3da80350497c9e38a430c781d100
Angshuman Paul <paulang1807@gmail.com>
2020-04-14 17:24:18 -0700

    Lifecycle methods and conditional rendering example

--- react-app/my-first-react-app/src/components/FirstComp.js
+++ react-app/my-first-react-app/src/components/FirstComp.js
@@ -5,13 +5,33 @@ import Footer from "./Footer"
 import StateSample from "./StateSample"
 
 class FirstComp extends React.Component {
+    constructor() {
+        super()
+        this.state = {
+            isLoading: true
+        }
+    }
+
+    componentDidMount() {
+        console.log("this is called after render")
+        setTimeout(() => {
+            this.setState({isLoading: false})
+        }, 1500)
+    }
+
     render() {
+        console.log("this runs first and then on any state change")
         return (
             <div>
-                <StateSample />
-                <Header />
-                <ListItems />
-                <Footer />
+                {this.state.isLoading ?
+                <h1>Loading ...</h1> :
+                <div>
+                    <StateSample />
+                    <Header />
+                    <ListItems />
+                    <Footer />
+                </div>
+                }
             </div>
         )
     }
