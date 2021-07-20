f19e53116846321dc2d35fd737ec24266fba60e3
Angshuman Paul <angshuman_paul2@intuit.com>
2021-07-20 16:26:20 -0700

    remove react-app -> paint-using-hooks

--- react-app/paint-using-hooks/components/Playground.js
+++
@@ -1,9 +0,0 @@
-import React from "react"
-
-function Playground() {
-    return (
-        <h1>Test</h1>
-    )
-}
-
-export default Playground
--- react-app/paint-using-hooks/index.js
+++
@@ -1,5 +0,0 @@
-import React from "react"
-import ReactDOM from "react-dom"
-import Playground from "./components/Playground"
-
-ReactDOM.render(<Playground />, document.getElementById("root"))
