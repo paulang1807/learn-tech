3f1b4dedb83fa5e2d52d2001e556bc12a099eee5
Angshuman Paul <angshuman_paul2@intuit.com>
2021-07-31 21:36:40 -0700

    useEffect Hook

--- react-app/basic_redux_example/src/hooks/HooksContainer1.js
+++ react-app/basic_redux_example/src/hooks/HooksContainer1.js
@@ -1,11 +1,20 @@
-import React, { useState } from 'react';
+import React, { useState , useEffect } from 'react';
 
 const HooksContainer1 = () => {
 
     const [test1, setTest1] = useState(2)
+    const [test2, setTest2] = useState("Loading...")
+
+    useEffect(() => {
+        setTimeout(() => setTest2("Initial Use Effect Change"), 3000)
+    }, [test1])
 
     const calcSquare = () => {
         setTest1(test1 * test1)
+    }
+
+    const changeValue = () => {
+        setTest2("New use Effect Value")
     }
 
     return (
@@ -13,7 +22,9 @@ const HooksContainer1 = () => {
         Hooks Container1
         <br />
         <button onClick={() => calcSquare()}>Get Square</button>
+        <button onClick={() => changeValue()}>Use Effect Test</button>
         <p>Value: {test1}</p>
+        <p>Value: {test2}</p>
       </div>
     )
 
